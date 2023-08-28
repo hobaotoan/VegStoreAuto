76577aa6b53e967a8fcf39751a7363c7ef0dd80c
DESKTOP-A8HQHM9\Admin <hobaotoan10aa2@gmail.com>
2023-08-28 22:19:29 +0700

    change somthing

--- src/test/java/cucumber/runner/TestRunner.java
+++ src/test/java/cucumber/runner/TestRunner.java
@@ -5,7 +5,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/Features", glue = { "cucumber.step" },
+@CucumberOptions(features = "src/test/resources/Features/LoginPage.feature", glue = { "cucumber.step" },
 		monochrome = true, plugin = { 
 						"pretty",
 						"json:target/cucumber.json", 
