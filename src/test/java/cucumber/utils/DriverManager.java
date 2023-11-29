5ed3d53252ef15950c14dcab5929a4456a7f94ca
DESKTOP-A8HQHM9\Admin <hobaotoan10aa2@gmail.com>
2023-11-29 22:34:43 +0700

    TESTING:Change to Chrome Driver

--- src/test/java/cucumber/utils/DriverManager.java
+++ src/test/java/cucumber/utils/DriverManager.java
@@ -66,7 +66,7 @@ public class DriverManager {
 			chromeOptions.addArguments("--disable-notifications");
 			chromeOptions.setExperimentalOption("excludeSwitches",Collections.singletonList("enable-automation"));
 
-			return new RemoteWebDriver(new URL("http://175.41.176.54:4444/wd/hub"), chromeOptions);
+			return new RemoteWebDriver(new URL("http://52.77.211.193:4444/wd/hub"), chromeOptions);
 //			return new ChromeDriver(chromeOptions);
 		default:
 			final FirefoxOptions ffOptions = new FirefoxOptions();
